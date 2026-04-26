d7a2638d10e13eeac780285f18eb3e4763729f7b
Dragoş <edragos33@gmail.com>
2026-04-26 20:17:35 +0200

    Navbar finised

--- assets/js/components/Menu.js
+++ assets/js/components/Menu.js
@@ -13,13 +13,29 @@ export default class Menu {
     init() {
         this.hideMenuItems();
         this.menuArrow.addEventListener("click", () => this.toggleMenu());
+
         if (this.closeBtn) {
             this.closeBtn.addEventListener("click", () => this.toggleMenu());
         }
+
         this.menuItems.forEach(item => {
             item.addEventListener("click", () => this.closeMenu());
         });
 
+    }
+
+    setMenuColor() {
+        if (!this.isOpen) {
+            let hex = '#';
+            const chars = '0123456789ABCDEF';
+            for (let i = 0; i < 6; i++) {
+                hex += chars[Math.floor(Math.random() * 16)];
+            }
+            this.menuContainer.style.backgroundColor = hex;
+        } else {
+            // Open → black
+            this.menuContainer.style.backgroundColor = "#1A1A1A";
+        }
     }
 
     toggleMenu() {
@@ -30,12 +46,14 @@ export default class Menu {
         this.menuContainer.style.width = "100vw";
         this.showMenuItems();
         this.isOpen = true;
+        this.setMenuColor();
     }
 
     closeMenu() {
-        this.menuContainer.style.width = "2px";
+        this.menuContainer.style.width = "5px";
         this.hideMenuItems();
         this.isOpen = false;
+        this.setMenuColor();
     }
 
     hideMenuItems() {
