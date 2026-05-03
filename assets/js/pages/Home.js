090f146a36cd7fa47de543a11f8045b1be9c0c06
Dragoş <edragos33@gmail.com>
2026-05-03 18:53:56 +0200

    main page done

--- assets/js/pages/Home.js
+++ assets/js/pages/Home.js
@@ -8,8 +8,8 @@ export default class Home {
       <section class="home">
         <div class="logo"><img src="assets/images/logo.webp" class="logo-img" alt="logo" /></div>
         <div class="info"> 
-          <h1>Dragoş ENACHE</h1>
-          <p>Apprenti en Marketing Digital & Webdesign</p>
+          <h1 class="home-title">Dragoş ENACHE</h1>
+          <p class="home-txt">Apprenti en Marketing Digital & Webdesign</p>
         </div>
       </section>
     `;
